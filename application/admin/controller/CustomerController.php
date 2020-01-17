6b691038bdb6ea2fccdb253ed4852d63cf1c1d94
candyliu <393512979@qq.com>
2020-01-17 11:14:32 +0800

    新增客户管理，用户模块方法

--- application/admin/controller/CustomerController.php
+++ application/admin/controller/CustomerController.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller;
 
 use app\common\controller\BaseController;
+use app\common\model\CustomerModel;
 use think\facade\Log;
 use think\facade\Session;
 
@@ -15,6 +16,36 @@ class CustomerController extends BaseController
      * @return \think\Response
      */
     public function index()
+    {
+        //
+        return $this->fetch();
+    }
+
+    public function getAll()
+    {
+        $page = \think\facade\Request::param('page', '1');
+        $limit = \think\facade\Request::param('limit', '10');
+        $search = \think\facade\Request::param('search', null);
+
+        $data = empty($search) ?
+            CustomerModel::order('createtime', 'desc')->paginate(intval($limit), false, ['page' => intval($page)])
+            : CustomerModel::where('wxname', 'like', '%' . $search . '%')
+                ->order('createtime', 'desc')->paginate(intval($limit), false, ['page' => intval($page)]);
+        if ($data->isEmpty())
+            $res = ['code' => 0, 'msg' => '', 'count' => 0, 'data' => []];
+        else {
+            $count = $data->total();
+            $res = ['code' => 0, 'msg' => '', 'count' => $count, 'data' => $data->items()];
+        }
+        return json($res);
+    }
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
     {
         //
         return $this->fetch();
